9e80cf82df01050bffc2f59443e3dae527571811
Thomas Corll <GitHub@tacincorporated.com>
2015-01-23 23:57:49 -0800

    about to add activity

--- app/src/main/java/com/tacincorporated/iwantadonut/MainActivity.java
+++ app/src/main/java/com/tacincorporated/iwantadonut/MainActivity.java
@@ -5,8 +5,13 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+
 
 public class MainActivity extends ActionBarActivity {
+    public final static String EXTRA_MESSAGE = "com.tacincorporated.IWantADonut.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +40,14 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
